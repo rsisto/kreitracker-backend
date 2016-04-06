39a7b1cf14570c346236b5aca78c8b4bd5cd8ee5
Jonathan <jonathanms33@google.com>
2016-04-06 17:49:10 -0300

    second commit

--- app/models/tracker_position.js
+++ app/models/tracker_position.js
@@ -3,7 +3,10 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var TrackerPositionSchema   = new Schema({
-    trackerId: String,
+    trackerId: {
+        type: Schema.ObjectId,
+        ref: 'Tracker'
+    },
     lat: String,
     lon: String,
     created_at: { type: Date, default: Date.now }
