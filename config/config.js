8416b733ab1a5c466baaa18b9a3b1d3548327e29
Jonathan <jonathanms33@google.com>
2016-04-15 19:24:25 -0300

    code redesign

--- config/config.js
+++ config/config.js
@@ -6,3 +6,10 @@ module.exports = _.extend(
     require(__dirname + '/../config/env/all.js'),
     require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {});
 
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://localhost:27017/kreitracker'
+
+};
+
