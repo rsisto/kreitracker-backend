73a6471e8efd298ac6763a5d5b85f07584d9befb
Jonathan <jonathanms33@google.com>
2016-06-22 18:32:52 -0300

    simulator fixed

--- tracker_simulator.sh
+++ tracker_simulator.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 echo "start telnet"
-telnet localhost 1337
-
+telnet localhost 1337 <<EOF
 ##,imei:359710047037420,A;
-imei:359710047037420,,,,F,,,,,,,,;
 imei:359710047037420,tracker,0809231929,13554900601,F,112909.397,A,2234.4669,N,11354.3287,E,0.11,;  
-     
+EOF 
 sleep 3	
 
 
